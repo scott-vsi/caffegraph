130ef51fba5a1ea7042fe5431f63125b87382ac9
Scott Richardson <scott.richardson@visionsystemsinc.com>
2018-04-20 18:19:16 +0000

    hack around caffe's non-standard batch normalization layer

--- init.lua
+++ init.lua
@@ -45,6 +45,7 @@ caffegraph.load_both = function(prototxt, caffemodel)
   -- transfer the parameters
   local noData = torch.FloatTensor():zero():cdata()
   local module_params = {}
+  local should_forward = true
   for j,nodes in ipairs(modmap) do
     local params = {}
     for i=1,#nodes do
@@ -61,25 +62,48 @@ caffegraph.load_both = function(prototxt, caffemodel)
       --  params[i*2] = module.bias and module.bias:cdata() or noData
       --end
 
-      if torch.isTypeOf(module, nn.BatchNormalization) then
-        -- HACK in dlib, a batch norm layer is swapped out at test time for an
-        -- affine layer (a scale and shift). there is not an affine layer in
-        -- torch, and this package ops to translate it to an nn.CMul and nn.Add.
-        -- however, this makes the next steps in the process harder. instead,
-        -- i hijack the affine transformation included (by definition) in the
-        -- nn.BatchNormalization layer (used while training) to perform an
-        -- affine transformation
-        -- TODO i could add a dummy nn.Scale layer which would allow me to create
-        -- .lua file and load the model definition and then hijack the batch norm
-        -- layer here
-        -- RE this would also involve re-writting the Scale layer in layers.cpp
-        module.running_mean:zero()
-        module.running_var:fill(1)
-      end
-      params[(i-1)*2+1] = module.weight and module.weight:cdata() or noData
-      params[i*2] = module.bias and module.bias:cdata() or noData
 
-      seq:add(module)
+      -- HACK caffe does not support the affine translation (scale and shift)
+      -- included as part of batch normalization in the original paper. caffe
+      -- suggests using a separate Scale layer, which includes a bias shift,
+      -- however, there is not a similar layer in torch. this package opts to
+      -- translate it to an nn.CMul and nn.Add. however, this makes the next
+      -- steps in the process harder. instead, i include two caffe BatchNorm
+      -- layers, each containing half the parameters of a torch (true)
+      -- nn.BatchNormalization layer and combine them here.
+
+      -- the faux-BatchNormalization layer (first)
+      if torch.isTypeOf(module, nn.BatchNormalization) and should_forward then
+        -- this node has only a single module and it is a nn.BatchNormalization
+        -- layer
+        assert(torch.isTypeOf(module, nn.BatchNormalization),
+       	    "expected this layer to be of type nn.BatchNormalization")
+
+        -- the next node has only a single module and it is a
+        -- nn.BatchNormalization layer
+        local nodes1 = modmap[j+1]
+        local module1 = nodes1[1].data.module
+        module1:float()
+        assert(torch.isTypeOf(module1, nn.BatchNormalization),
+           "expected the next layer to be of type nn.BatchNormalization")
+
+        -- reach forward and fill out the running mean and variance of the
+        -- nn.BatchNormalization layer
+        params[(i-1)*2+1] = module1.running_mean and module1.running_mean:cdata() or noData
+        params[i*2] = module1.running_var and module1.running_var:cdata() or noData
+
+        should_forward = false
+      else
+        -- the BatchNormalization layer (second)
+        if torch.isTypeOf(module, nn.BatchNormalization) then
+          should_forward = true
+        end
+
+        params[(i-1)*2+1] = module.weight and module.weight:cdata() or noData
+        params[i*2] = module.bias and module.bias:cdata() or noData
+
+        seq:add(module)
+      end
     end
     module_params[j] = ffi.new('THFloatTensor*['..#params..']', params)
   end
@@ -91,3 +115,4 @@ caffegraph.load_both = function(prototxt, caffemodel)
 end
 
 return caffegraph
+
